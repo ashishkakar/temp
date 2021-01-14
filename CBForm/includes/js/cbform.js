f0433947d57e439f09e877aee8e5dcc7a9d3a787
Ashish <ashish_kakar@hotmail.com>
2021-01-14 14:05:55 -0600

    Basic setup

---
+++ CBForm/includes/js/cbform.js
@@ -0,0 +1,7 @@
+//set no exec for includes
+//route all through indes
+//enable strict
+//enable dev env
+//create error class
+//create router
+//create init to load requirements
---
+++ CBForm/views/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="includes/css/cbform.css">
+<script src="includes/js/cbform.js"></script>
+<title></title>
+</head>
+<body>
